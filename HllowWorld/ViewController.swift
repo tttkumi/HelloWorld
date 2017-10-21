85f157de3977c6f51e84e35d5b3e587fa95d369e
tttkumi <tsrpm031@yahoo.co.jp>
2017-10-21 07:36:54 +0700

    新しく作成

---
+++ HllowWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HllowWorld
+//
+//  Created by 前田 匠 on 2017/10/20.
+//  Copyright © 2017年 takumi.maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
